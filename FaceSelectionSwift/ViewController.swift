3e760cee64ce269d5315f8a1a307248867a65136
Joseph Kubiak <joseph.kubiak@gmail.com>
2018-06-24 16:26:56 -0700

    Initial Commit

---
+++ FaceSelectionSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceSelectionSwift
+//
+//  Created by Joseph Kubiak on 6/24/18.
+//  Copyright © 2018 Joseph Kubiak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
